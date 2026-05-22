1127aab8743932d4df50f09aba1798cff879fb2e
Jason <verymeanguy13@gmail.com>
2026-05-23 04:44:15 +0800

    test: disable middleware temporarily

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,5 @@
-import createMiddleware from 'next-intl/middleware';
-
-export default createMiddleware({
-  locales:       ['zh', 'en'],
-  defaultLocale: 'zh',
-});
+export default function middleware() {}
 
 export const config = {
-  // Match all pathnames except for API routes, static files, and Next.js internals
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: [],
 };
